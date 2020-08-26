902423bed29ba6085fa5fa49d05ebc187d51e7d4
Kloong1 <dkdlsmsm98@gmail.com>
2020-08-26 19:38:34 +0900

    Updates

---
+++ precache-manifest.b7f5c26d40f3e28e1aa9e8588bc45c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e4f3a082818966005543e1201af091",
+    "url": "/Kloong1/movie_app_2020/index.html"
+  },
+  {
+    "revision": "5c21019dfdecb342603c",
+    "url": "/Kloong1/movie_app_2020/static/css/main.818fef33.chunk.css"
+  },
+  {
+    "revision": "6344eb9692903ef59200",
+    "url": "/Kloong1/movie_app_2020/static/js/2.63fe6cf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kloong1/movie_app_2020/static/js/2.63fe6cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c21019dfdecb342603c",
+    "url": "/Kloong1/movie_app_2020/static/js/main.25264f95.chunk.js"
+  },
+  {
+    "revision": "5092b118f346d5b5a7a7",
+    "url": "/Kloong1/movie_app_2020/static/js/runtime-main.42269e48.js"
+  }
+]);
